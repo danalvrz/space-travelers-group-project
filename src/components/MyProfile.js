4dd6a3f5e621444ebc0eb612e9ddcef70ff45715
danalvrz <dante.alvrz@gmail.com>
2022-02-01 11:50:29 -0600

    Added missions feature to MyProfile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function MyProfile() {
+  const missions = useSelector((state) => state.missions);
+  const myMissionsArr = [];
+
+  const MyMissions = () => {
+    missions.forEach((mission) => {
+      if (mission.member) {
+        myMissionsArr.push(
+          <tr key={mission.mission_id} className="border h-14">
+            <td className="pl-5">{mission.mission_name}</td>
+          </tr>,
+        );
+      }
+    });
+    return myMissionsArr;
+  };
+
   return (
-    <div>
-      <h1>My Profile</h1>
+    <div className="flex mx-20">
+      <div className="missions container">
+        <h2 className="font-bold text-3xl pb-3">My Missions</h2>
+        <table className="border w-11/12">
+          <tbody>
+            <MyMissions />
+          </tbody>
+        </table>
+      </div>
+      <div className="rockets container">
+        <h2 className="font-bold text-3xl pb-3">My Rockets</h2>
+        <table className="border w-11/12">
+          <tbody>
+            {/* the next <tr> element should be replaced with the correct rockets component */}
+            <tr className="border h-14">
+              <td className="pl-5">TEST</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
