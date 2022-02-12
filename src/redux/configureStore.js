72328b0b645969f25bae542735dbae49ecfec6d9
Maclenn77 <maclenn77@gmail.com>
2022-02-12 16:07:08 -0600

    Add meta image

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,6 +4,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 
 const composedEnhancer = compose(applyMiddleware(logger), applyMiddleware(thunk));
+
 const store = createStore(
   rootReducer,
   composedEnhancer,
